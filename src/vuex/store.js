bc57fa6954a09faf57db6411c9495a39c746e62b
kotori yang <yzjkid9@gmail.com>
2021-07-07 19:27:28 +0800

    :art: used cookies instead of localstorage

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -12,10 +12,29 @@ import Vue from "vue"
 import Vuex from "vuex"
 Vue.use(Vuex)
 
+const setCookie = (name, value) => {
+  let date = new Date();
+  date.setTime(date.getTime() + (5 * 60 * 1000))
+  const expires = "; expires=" + date.toUTCString()
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+const getCookie = (name) => {
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(';');
+  for(let i=0;i < ca.length;i++) {
+    let c = ca[i];
+    while (c.charAt(0)===' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+
 
 // init state
 const state = {
-  druglikeness_result: JSON.parse(localStorage.getItem("druglikeness_result"))
+  druglikeness_result: JSON.parse(getCookie("druglikeness_result")),
 }
 
 const actions = {
@@ -27,7 +46,7 @@ const actions = {
 const mutations = {
   UPDATE(state, value) {
     state.druglikeness_result = value
-    localStorage.setItem("druglikeness_result", JSON.stringify(value))
+    setCookie("druglikeness_result", JSON.stringify(value))
   }
 }
 
